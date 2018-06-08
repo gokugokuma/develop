2fb58ff36531ca667420aa759b4d281a0adddcda
masahiro imoto <19920423.m.i@gmail.com>
2018-06-08 14:23:38 +0900

    add first commit

---
+++ start-web-project/src/main/java/com/develop/web/StartWebProjectApplication.java
@@ -0,0 +1,12 @@
+package com.develop.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartWebProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartWebProjectApplication.class, args);
+	}
+}
---
+++ start-web-project/src/test/java/com/develop/web/StartWebProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.develop.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StartWebProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
